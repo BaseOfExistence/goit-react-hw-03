a5ef0ab8af69b85a8242ba46c6829d71ad538154
BaseOfExistence <139703947+BaseOfExistence@users.noreply.github.com>
2024-03-26 15:29:54 +0200

    hw3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,54 @@
-import Profile from "../Profile/Profile.jsx"
-import FriendList from "../FriendList/FriendList.jsx"
-import TransactionHistory from "../TransactionHistory/TransactionHistory.jsx"
-import userData from "../../userData.json"
-import friends from"../../friends.json"
-import transactions from "../../transactions.json"
+import { useState } from "react"
+import { useEffect } from "react"
+import ContactList from "../ContactList/ContactList"
+import SearchBox from "../SearchBox/SearchBox"
+import ContactForm from "../ContactForm/ContactForm"
+import css from "./App.module.css"
 
 export default function App() {
+  const [contacts, setContacts] = useState(() => {
+    if (localStorage.getItem("contacts") !== null) {
+      return (
+        JSON.parse(localStorage.getItem("contacts"))
+      )
+    }
+    return (
+      [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    )
+  })
+  const [filter, setFilter] = useState("")
+  const AddContact = (newContact) => {
+    setContacts((contacts) => {
+      return (
+        [ ...contacts, newContact]
+      )
+    })
+  }
+  const DeletContact = (contactId) => {
+    setContacts((contacts) => {
+      return (
+        contacts.filter((contact) => contact.id !== contactId)
+      )
+    })
+  }
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts))
+  }, [contacts])
+  const visibleContacts = contacts.filter((contact) => {
+    return contact.name.toLowerCase().includes(filter.toLowerCase())
+  })
   return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
+    <div className={css.app}>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={AddContact} />
+      <SearchBox value={filter} onChange={setFilter} />
+      <ContactList contacts={visibleContacts} deleteContact={DeletContact} />
+    </div>
   )
 }
 
